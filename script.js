fbcf3d69d9c227693d3eafaf68c25be79b4220ee
Lorenzo Verardo <lorenzo.verardo@outlook.it>
2024-05-13 19:38:48 +0200

    Improve styling

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ $(function () {
 	$(window).focus(function () {
 		window.isActive = true;
 		updatePageStatus();
+		updatePageTitle();
 	});
 	$(window).blur(function () {
 		window.isActive = false;
 		updatePageStatus();
+		updatePageTitle();
 	});
 	updatePageStatus();
 });
@@ -15,15 +17,20 @@ function updatePageStatus() {
 	var statusElement = $("#status");
 	if (!window.isActive) {
 		statusElement
-			.text("No, the page is not active")
+			.text("No, the page is not active!")
 			.addClass("inactive")
 			.removeClass("active");
 		$("body").css("background-color", "red");
 	} else {
 		statusElement
-			.text("Yes, the page is active")
+			.text("Yes, the page is active!")
 			.addClass("active")
 			.removeClass("inactive");
 		$("body").css("background-color", "green");
 	}
+}
+
+function updatePageTitle() {
+	var newTitle = window.isActive ? "Active 🙂" : "Inactive 🙁";
+	$("title").text(newTitle);
 }
